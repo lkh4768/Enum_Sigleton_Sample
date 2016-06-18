c45b1e6db9ba3dbfe3a0f9e9fbd65667764512ff
lkh4768 <lkh5510@gmail.com>
2016-06-18 16:04:12 +0900

    init commit

---
+++ Enum_Sigleton/src/Cilent.java
@@ -0,0 +1,6 @@
+
+public class Cilent {
+	public static void main(String[] args) {
+		System.out.println(EnumSigleton.INSTANS.toString());
+	}
+}
---
+++ Enum_Sigleton/src/EnumSigleton.java
@@ -0,0 +1,11 @@
+
+public enum EnumSigleton {
+	INSTANS;
+
+	private String id = "000000";
+	private String name = "lee";
+
+	public String toString() {
+		return ("id : " + id + " name : " + name);
+	}
+}
